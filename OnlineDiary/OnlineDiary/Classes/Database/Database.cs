2f5432e172d4736e1b63c4c7839375a1429cfbcd
diogoalves86 <diogoalvessilva86@gmail.com>
2015-10-29 15:50:35 -0300

    Implementando arquivos do banco de dados

--- OnlineDiary/OnlineDiary/Classes/Database/Database.cs
+++ OnlineDiary/OnlineDiary/Classes/Database/Database.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace OnlineDiary.Classes.Database
 {
     public class Database
     {
+        private string strConexao = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        private SqlConnection conexao;
+        private SqlCommand comando;
+
+        public Database()
+        {
+            conexao = new SqlConnection(strConexao);
+        }
+
+        public SqlDataReader QueryReader(string strComando)
+        {
+            comando = new SqlCommand();
+            comando.CommandText = strComando;
+            return comando.ExecuteReader();
+        }
+
+        public int QueryRow(string strComando)
+        {
+            comando = new SqlCommand();
+            return comando.ExecuteNonQuery();
+        }
     }
 }
