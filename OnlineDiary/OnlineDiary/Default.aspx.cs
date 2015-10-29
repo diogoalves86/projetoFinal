1eb00ad4a97bb873496d114589c9ec8893317f0a
Nathália <naty_souzarj@hotmail.com>
2015-10-29 17:45:24 -0300

    Login html e css modificados

--- OnlineDiary/OnlineDiary/Default.aspx.cs
+++ OnlineDiary/OnlineDiary/Default.aspx.cs
@@ -11,7 +11,10 @@ namespace OnlineDiary
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!this.User.Identity.IsAuthenticated)
+            {
+                
+            }
         }
     }
 }
