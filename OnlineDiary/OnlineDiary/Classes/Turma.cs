84d77b6f84bdc317066614a7f6d9c82bedc98fc6
diogoalves86 <diogoalvessilva86@gmail.com>
2015-10-29 16:13:00 -0300

    Implementando classes de modelo

--- OnlineDiary/OnlineDiary/Classes/Turma.cs
+++ OnlineDiary/OnlineDiary/Classes/Turma.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace OnlineDiary.Classes
 {
     public class Turma
     {
         private Database banco = new Database();
+
+
+        public void PegarTurma()
+        {
+            Hashtable alunos = new Hashtable();
+            SqlDataReader reader = banco.QueryReader("SELECT * FROM Turma");
+            while(reader.Read())
+            {
+                alunos["aa"] = "a";
+            }
+        }
     }
 }
