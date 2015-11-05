ee9bddf8f2d1462a54fbb75067c11d4a5381b605
Diogo Alves <diogoalvessilva86@gmail.com>
2015-11-05 14:47:51 -0300

    Implementando regras de acesso

--- OnlineDiary/OnlineDiary/Regras/ManageRoles.aspx
+++ OnlineDiary/OnlineDiary/Regras/ManageRoles.aspx
@@ -1,7 +1,7 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ManageRoles.aspx.cs" Inherits="OnlineDiary.Roles.ManageRoles" %>
-<asp:Content ID="Content2" ContentPlaceHolderID="HeadContent" Runat="Server">    
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">    
     <b>Create a New Role: </b>
     <asp:TextBox ID="RoleName" Runat="server"></asp:TextBox>
     <br />
-    <asp:Button ID="CreateRoleButton" Runat="server" Text="Create Role" />
+    <asp:Button ID="CreateRoleButton" Runat="server" Text="Create Role" OnClick="CreateRoleButton_Click" />
 </asp:Content>
---
+++ OnlineDiary/OnlineDiary/Regras/ManageRoles.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+namespace OnlineDiary
+{
+    public partial class ManageRoles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void CreateRoleButton_Click(object sender, EventArgs e) 
+        { 
+            string newRoleName = RoleName.Text.Trim(); 
+            if (!Roles.RoleExists(newRoleName)) 
+                Roles.CreateRole(newRoleName); 
+            RoleName.Text = string.Empty; 
+        }
+    }
+}
